63d0553ee21696e94ecf4d81f414d0b5376a2a88
ethanlanier <ethan.lanier@icloud.com>
2024-10-01 20:45:41 -0400

    Renders 1 object but no errors

--- CSC345P1/init-buffers.js
+++ CSC345P1/init-buffers.js
@@ -1,16 +1,18 @@
 function initBuffers(gl) {
-    const positionBuffer = initPositionBuffer(gl);
+    const skyPosBuffer = initSkyPositionBuffer(gl);
+    const groundPosBuffer = initGroundPositionBuffer(gl);
   
-    const colorBuffer = initColorBuffer(gl);
+    const skyColorBuffer = initSkyColorBuffer(gl);
+    const groundColorBuffer = initGroundColorBuffer(gl);
 
     return {
-        position: positionBuffer,
-        color: colorBuffer,
+        position: skyPosBuffer,
+        color: skyColorBuffer,
     };
 
   }
   
-  function initPositionBuffer(gl) {
+  function initSkyPositionBuffer(gl) {
     //create a buffer for the square's positions
     const positionBuffer = gl.createBuffer();
   
@@ -18,7 +20,7 @@ function initBuffers(gl) {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   
     //now create an array of positions for the square.
-    const sky = [3.5, 3.0, -3.5, 3.0, 3.5, -3.0, -3.5, -3.0];
+    const sky = [3.5, 3.0, -3.5, 3.0, 3.5, 0, -3.5, 0];
     
   
     //now pass the list of positions into WebGL to build the shape by creating a Float32Array from the JavaScript array, then use it to fill the current buffer
@@ -26,25 +28,46 @@ function initBuffers(gl) {
   
     return positionBuffer;
   }
+
+  function initGroundPositionBuffer(gl) {
+    //create a buffer for the square's positions
+    const positionBuffer = gl.createBuffer();
+  
+    //select the positionBuffer as the one to apply buffer operations to from here out.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  
+    //now create an array of positions for the square.
+    const sky = [3.5, 0.0, -3.5, 0.0, 3.5, -3.0, -3.5, -3.0];
+    
+  
+    //now pass the list of positions into WebGL to build the shape by creating a Float32Array from the JavaScript array, then use it to fill the current buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sky), gl.STATIC_DRAW);
+  
+    return positionBuffer;
+  }
+
     //create buffer for sky color
-  function initColorBuffer(gl) {
+  function initSkyColorBuffer(gl) {
     const colors = [
-      1.0,
-      1.0,
-      1.0,
-      1.0, // white
-      1.0,
-      0.0,
-      0.0,
-      1.0, // red
-      0.0,
-      1.0,
-      0.0,
-      1.0, // green
-      0.0,
-      0.0,
-      1.0,
-      1.0, // blue
+      0.0, 0.0, 1.0, 1.0, // blue
+      0.0, 0.0, 1.0, 1.0, // blue
+      0.0, 0.0, 1.0, 1.0, // blue
+      0.0, 0.0, 1.0, 1.0, // blue
+    ];
+  
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  
+    return colorBuffer;
+  }
+
+  function initGroundColorBuffer(gl) {
+    const colors = [
+      0.0, 1.0, 0.0, 1.0, // green
+      0.0, 1.0, 0.0, 1.0, 
+      0.0, 1.0, 0.0, 1.0, 
+      0.0, 1.0, 0.0, 1.0,
     ];
   
     const colorBuffer = gl.createBuffer();
